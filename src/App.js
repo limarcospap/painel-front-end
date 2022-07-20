940d90639ec85f4b2300dba66a7171262c93f5f6
Marcos Paulo <limarcospap@gmail.com>
2022-07-20 11:45:15 -0300

    new logs screen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import 'handsontable/dist/handsontable.full.css';
 import './App.css';
 
 export default class App extends Component {
--- src/components/Teste.js
+++ src/components/Teste.js
@@ -1,20 +1,48 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import useWebSocket from "react-use-websocket";
+import { HotTable, HotColumn } from "@handsontable/react";
+import { registerAllModules } from "handsontable/registry";
+
+// register Handsontable's modules
+registerAllModules();
+
+const hotData = [
+  ["", "Tesla", "Volvo", "Toyota", "Honda"],
+  ["2020", 10, 11, 12, 13],
+  ["2021", 20, 11, 14, 13],
+  ["2022", 30, 15, 12, 13],
+];
+
 export default function Teste(props) {
-  const [mensagem, setMensagem] = useState("Hei hei");
+  const websocketURL = "ws://localhost:8087/api/logs/refresh-logs";
+  const [obj, setObj] = useState([{ name: "mp" }]);
+  const { sendMessage, lastMessage } = useWebSocket(websocketURL, {
+    onOpen: () => console.log("Connected to teste"),
+    onMessage: (message) => {
+      setObj(JSON.parse(message.data));
+    },
+    shouldReconnect: (closeEvent) => true,
+  });
 
-  const { lastJsonMessage, sendMessage } = useWebSocket(
-    "ws://localhost:8089/api/logs/refresh-logs",
-    {
-      onOpen: () => console.log("Connected to teste"),
-      onMessage: (lastJsonMessage) => {
-        if (lastJsonMessage) {
-          setMensagem(lastJsonMessage?.data);
-        }
-      },
-      shouldReconnect: (closeEvent) => true,
-    }
+
+
+  return (
+    <HotTable
+      data={obj}
+      colHeaders={true}
+      rowHeaders={false}
+      width="auto"
+      height="300"
+    >
+      <HotColumn data="_id" title="ID"/>
+      <HotColumn data="Proto_tcp" title="TCP?" />
+      <HotColumn data="Proto_udp" title="UDP?" />
+      <HotColumn data="Dur" title="Duração"/>
+      <HotColumn data="TotBytes" title="Total Bytes"/>
+      <HotColumn data="TotPkts" title="Total Pacotes"/>
+      <HotColumn data="dTos" title="ToS Destino"/>
+      <HotColumn data="sTos" title="Tos Fonte"/>
+      <HotColumn data="SrcBytes" title="Bytes Fonte"/>
+    </HotTable>
   );
-
-  return <span>Este é um teste {mensagem} </span>;
 }
